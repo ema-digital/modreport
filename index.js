5dc07ed71167b713d7035bdb7dc5765202eceb29
egardner <egardner@mower.com>
2015-09-11 13:37:41 -0400

    updating file path to be relative to cwd. adding node env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var commandopts = require('./lib/command').command(process.argv),
   recursive = require('recursive-readdir'),
   fs = require('fs'),
@@ -5,7 +6,7 @@ var commandopts = require('./lib/command').command(process.argv),
   moment = require('moment');
 
 var modsince = function(err, ms) { 
-  recursive(process.cwd(), ['node_modules', '.git', '.sass-cache', 'foo.cs', '*.html'], function (err, files) {
+  recursive(process.cwd(), ['node_modules', '.git', '.sass-cache', '.svn', '.bak'], function (err, files) {
     if (err) {
       console.log(err);
     }
@@ -23,8 +24,10 @@ var modsince = function(err, ms) {
             if (stats.isFile()) {
               var mod_time = new Date(stats.mtime).getTime();
             
-              if ( moment( mod_time ).isAfter( compare_time ) ) {
-                console.log( path.normalize(element) );
+              if (moment(mod_time).isAfter(compare_time) ) {
+                var file = path.normalize(element);
+                file = file.replace(path.normalize(process.cwd()), '.');
+                console.log(file);
               }
             }
           });
